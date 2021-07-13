78a338ff49adb71639b5b3a1b704fccc11ce3cd0
TimGrohmann <Tim.Grohmann@deutschebahn.com>
2021-07-13 11:57:10 +0200

    added licenses

--- src/transactions/AbandonedCartTransactionService.php
+++ src/transactions/AbandonedCartTransactionService.php
@@ -1,4 +1,27 @@
 <?php
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2013-2021 XQueue GmbH
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 namespace PrestaShop\Module\XQMaileon\Transactions;
 
@@ -99,7 +122,9 @@ class AbandonedCartTransactionService extends AbstractTransactionService impleme
         $customerForCart = new \Customer($abandonedCart->id_customer);
 
         # do not process cart if customer does not exist (anymore)
-        if (empty($customerForCart->email)) return false;
+        if (empty($customerForCart->email)) {
+            return false;
+        }
 
         # only send abandoned cart notification if customer has opted in to emails or
         # double opt in was not configured in module settings
